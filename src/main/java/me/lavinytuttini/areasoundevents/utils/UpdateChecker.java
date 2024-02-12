6983fd9728040921ca9ee18c8818dfd14b4089b2
Laviny Tuttini <lavin.sti@gmail.com>
2024-02-12 16:15:42 +0100

    UpdateChecker class code optimization.

--- src/main/java/me/lavinytuttini/areasoundevents/utils/UpdateChecker.java
+++ src/main/java/me/lavinytuttini/areasoundevents/utils/UpdateChecker.java
@@ -5,11 +5,13 @@ import org.bukkit.Bukkit;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
 import java.util.function.Consumer;
 
 public class UpdateChecker {
+    private static final String UPDATE_URL = "https://api.spigotmc.org/legacy/update.php?resource=";
     private final AreaSoundEvents areaSoundEvents;
     private final int resourceId;
 
@@ -20,12 +22,19 @@ public class UpdateChecker {
 
     public void getVersion(final Consumer<String> consumer) {
         Bukkit.getScheduler().runTaskAsynchronously(this.areaSoundEvents, () -> {
-            try (InputStream inputStream = new URL("https://api.spigotmc.org/legacy/update.php?resource=" + this.resourceId).openStream(); Scanner scanner = new Scanner(inputStream)) {
-                if (scanner.hasNext()) {
-                    consumer.accept(scanner.next());
+            try {
+                URL url = new URL(UPDATE_URL + this.resourceId);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                try (InputStream inputStream = connection.getInputStream();
+                     Scanner scanner = new Scanner(inputStream)) {
+                    if (scanner.hasNext()) {
+                        consumer.accept(scanner.next());
+                    }
                 }
-            } catch (IOException exception) {
-                areaSoundEvents.getLogger().info("Unable to check for updates: " + exception.getMessage());
+                connection.disconnect();
+            } catch (IOException ex) {
+                areaSoundEvents.getLogger().warning("Error checking for updates: " + ex.getMessage());
             }
         });
     }
